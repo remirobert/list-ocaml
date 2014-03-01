3c86cff5b0bdf5208cd55568c5d348c5f1c4c8a8
robert_r <remi.robert@epitech.eu>
2014-03-01 09:44:53 +0100

    add append function

--- list.ml
+++ list.ml
@@ -31,7 +31,6 @@ let tl (list:'a my_list) : 'a my_list =
   | Item (data, Empty) -> Empty
   | Item (data, next) -> next
 
-
 let rec nth (list:'a my_list) (nb:int) : 'a = 
   match nb with
   | i when nb > length list -> failwith "The list is too short"
@@ -42,24 +41,16 @@ let rec nth (list:'a my_list) (nb:int) : 'a =
 	if nb == 0 
 	then data
 	else nth next (nb - 1)
-       
+
+let rec append (list1:'a my_list) (list2:'a my_list) : 'a my_list = 
+  match list1 with
+  | Empty -> list2
+  | Item (data, next) -> Item (data, append next list2)
 
 let l = Item ("salut", Item ("remi double", Item ("salut1", Empty)));;
+let l2 = Item ("toto", Item ("123", Item ("ok ca marche", Empty)));;
 
-print_endline "test my_list";;
+let append_list = append l l2;;
 let size = length l;;
-
-display_content_list l;;
-
-print_int size;;
-
-print_endline "test tl"
-let last = tl l;;
-display_content_list last;;
-
-let elem = nth l 0;;
-
-print_endline "";;
-print_endline "try get elem at pos";;
-print_endline "";;
-let tmp = nth Empty 2;;
+let size1 = length l2;;
+let sizea = length append_list;;
